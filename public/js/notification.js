2236d488a8ccdfa0ac1fd1adae0d64c2445506ae
RaphaelHartner <raphael.hartner@gmx.at>
2016-04-05 22:55:09 +0200

    vibration

--- public/js/notification.js
+++ public/js/notification.js
@@ -1,27 +1,30 @@
 function usageNotification() {
 
-  navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-  if (!("Notification" in window)) {
-    alert("Benachrichtigungen werden vom Browser nicht unterstützt");
-  }
-  else if (Notification.permission === "granted") {
-    var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
-        "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Gebühren für Datendienste anfallen können.");
-    if (navigator.vibrate) {
-      navigator.vibrate([500, 500, 500]);
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+    if (!("Notification" in window)) {
+        alert("Benachrichtigungen werden vom Browser nicht unterstützt");
     }
-  }
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      if (permission === "granted") {
+    else if (Notification.permission === "granted") {
+        if (navigator.vibrate) {
+            navigator.vibrate([500, 500, 500]);
+        }
+
         var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
             "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Gebühren für Datendienste anfallen können.");
-          if (navigator.vibrate) {
-              navigator.vibrate([500, 500, 500]);
-          }
-      }
-    });
-  }
+    }
+    else if (Notification.permission !== 'denied'){
+
+        Notification.requestPermission(function (permission) {
+            if (permission === "granted") {
+
+                if (navigator.vibrate) {
+                    navigator.vibrate([500, 500, 500]);
+                }
+                var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
+                    "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Gebühren für Datendienste anfallen können.");
+            }
+        });
+    }
 }
 
 $(document).ready(usageNotification);
