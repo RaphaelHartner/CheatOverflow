fcd3788fbd78ac33b6b88ada6c30af4663de4f8e
adelmang14 <georgchristian.adelmann@edu.fh-joanneum.at>
2016-04-05 22:20:15 +0200

    notifications and vibration

---
+++ public/js/notification.js
@@ -0,0 +1,25 @@
+function usageNotification() {
+
+  navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+  if (!("Notification" in window)) {
+    alert("Benachrichtigungen werden vom Browser nicht unterstützt");
+  }
+  else if (Notification.permission === "granted") {
+    var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
+        "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Geb�hren f�r Datendienste anfallen k�nnen.");
+    if (navigator.vibrate) {
+      navigator.vibrate([500, 500, 500]);
+    }
+  }
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
+            "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Geb�hren f�r Datendienste anfallen k�nnen.");
+        navigator.vibrate([500, 500, 500]);
+      }
+    });
+  }
+}
+
+$(document).ready(usageNotification);
