23d12981b6af18ea0e1467854ad8142873d1767a
adelmang14 <georgchristian.adelmann@edu.fh-joanneum.at>
2016-04-05 22:32:02 +0200

    notifications and vibration

--- public/js/notification.js
+++ public/js/notification.js
@@ -6,7 +6,7 @@ function usageNotification() {
   }
   else if (Notification.permission === "granted") {
     var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
-        "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Geb�hren f�r Datendienste anfallen k�nnen.");
+        "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Gebühren für Datendienste anfallen können.");
     if (navigator.vibrate) {
       navigator.vibrate([500, 500, 500]);
     }
@@ -15,8 +15,10 @@ function usageNotification() {
     Notification.requestPermission(function (permission) {
       if (permission === "granted") {
         var notification = new Notification("Im Hintergrund werden Daten gesendet und empfangen. " +
-            "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Geb�hren f�r Datendienste anfallen k�nnen.");
-        navigator.vibrate([500, 500, 500]);
+            "Falls Sie mit einem Mobiltelefon online sind, beachten Sie dass eventuell Gebühren für Datendienste anfallen können.");
+          if (navigator.vibrate) {
+              navigator.vibrate([500, 500, 500]);
+          }
       }
     });
   }
